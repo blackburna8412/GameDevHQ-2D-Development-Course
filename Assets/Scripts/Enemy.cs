ae5bd9a3464186690c336f41dd33e3eb46e181ec
Alex Blackburn <blackburna8412@outlook.com>
2020-06-08 12:18:48 -0700

    Initial implementation of Enemy Behavior

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private float _movementSpeed = 4f;
+    private float yMin = -6f, yMax = 7f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down * _movementSpeed * Time.deltaTime);
+
+        if(transform.position.y < yMin)
+        {
+            float randomXRange = Random.Range(-9.37f, 9.37f);
+            transform.position = new Vector3(randomXRange, yMax, transform.position.z);
+        }
     }
 }
